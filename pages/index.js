fb8fb443cd657b04cf28bd6d490e88b66e08690f
Moewperson5 <84649911+Moewperson5@users.noreply.github.com>
2022-04-04 09:01:08 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
       <main>
         <Header title="Welcome to ACgames!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          A & C Games is your number one stop shop for retro classics, rare collectibles, and hot new titles
         </p>
       </main>
 
